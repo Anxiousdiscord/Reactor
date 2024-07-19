503b32a016aac4679639dc3d5a8b5ae72d92160d
Anxiousdiscord <anxiousdiscord1@gmail.com>
2024-07-19 09:55:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,66 +9,8 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@1157648170159976479>')) {
-    message.react('🇲');
+  if (message.content.toLowerCase().includes('<@User Id>')) {
     message.react('🇦');
-    message.react('🇭');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@707111534689648760>')) {
-    message.react('🇵');
-    message.react('🇷');
-    message.react('🇦');
-    message.react('🇹');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@1233359097357799558>')) {
-    message.react('🇸');
-    message.react('🇰');
-    message.react('🇾');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@1238877972887244881>')) {
-    message.react('🇫');
-    message.react('🇨');
-    message.react('🇰');
-    message.react('🇺');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@1103086081689256009>')) {
-    message.react('🦈');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@1254990095195574273>')) {
-    message.react('6️⃣');
-    message.react('🇮');
-    message.react('🇽');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@1155500922361884733>')) {
-    message.react('🇩🇪');
-    message.react('🍻');
-    message.react('🧌');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('<@1241379023976923171>')) {
-    message.react('🇨');
-    message.react('🇷');
-    message.react('7️⃣');
   }
 });
 
